3c344d7beeba6ac7b0246f39a2447b7444b8ee45
Arpita Sinha <sinha.arpita@gmail.com>
2019-09-24 17:36:04 -0700

    changes

--- adventure/admin.py
+++ adventure/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Room
+from .models import Player
+admin.site.register(Room)
+admin.site.register(Player)
